d68f9838c6c666619296c063584f99c00fe55384
sprudnikov <sprudnikov@yandex.ru>
2020-02-20 14:22:21 +0300

    ok

--- WebStore.Domain/Entities/Base/Interfaces/INamedEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebStore.Domain.Entities.Base.Interfaces
-{
-    /// <summary>Именованная сущность</summary>
-    public interface INamedEntity : IBaseEntity
-    {
-        /// <summary>Имя</summary>
-        string Name { get; set; }
-    }
-}
--- WebStore.Domain/Entities/Base/Interfaces/IOrderedEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebStore.Domain.Entities.Base.Interfaces
-{
-    /// <summary>Упорядочиваемая сущность</summary>
-    public interface IOrderedEntity : IBaseEntity
-    {
-        /// <summary>Порядковый номер</summary>
-        int Order { get; set; }
-    }
-}
